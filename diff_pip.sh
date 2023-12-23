2326a6822f78c9bea8ec928a2c9d25ed2763af09
Zapta <zapta@users.noreply.github.com>
2023-12-23 11:16:37 -0800

    Added a top level gitignore

--- diff_pip.sh
+++ diff_pip.sh
@@ -3,10 +3,10 @@
 # A script to diff the pip installed serial_packets against the local git repo
 
 #pip_pkg="/c/Users/user/AppData/Roaming/Python/Python311/site-packages/serial_packets"
-pip_pkg="/c/Users/user/AppData/Local/Programs/Python/Python312/Lib/site-packages/serial_packets"
+pip_pkg="/c/Users/user/AppData/Local/Programs/Python/Python312/Lib/site-packages/i2c_adapter"
 
 
-git_pkg="./src/serial_packets"
+git_pkg="./src/i2c_adapter"
 
 #ls $pip_pkg
 #ls $git_pkg
