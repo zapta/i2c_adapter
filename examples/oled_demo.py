034258227fddad5c17cffc40d24ad007e55ec5a9
Zapta <zapta@users.noreply.github.com>
2023-12-29 16:08:58 -0800

    More progress on the luma example

--- examples/oled_demo.py
+++ examples/oled_demo.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from luma.core.render import canvas
 from PIL import ImageFont
 import time
+from typing import List
 
 # Relevant links
 # https://luma-oled.readthedocs.io/en/latest/
@@ -13,6 +14,8 @@ import time
 # https://github.com/rm-hull/luma.examples/tree/master/examples
 # https://github.com/rm-hull/luma.examples/blob/master/examples/sys_info.py
 
+# OLED display I2C address.
+addr = 0x3c
 
 class MyLumaSerial:
     """I2C implementation of the Luma 'Serial' interface."""
@@ -23,16 +26,24 @@ class MyLumaSerial:
 
     def command(self, *cmd):
         """Send command bytes."""
+        assert isinstance(cmd, tuple)
+        assert isinstance(cmd[0], int)
         assert len(cmd) <= 32
+        cmd_bytes = bytearray(list(cmd))
+        assert len(cmd_bytes) == len(cmd)
+        print(f"cmd ({len(cmd_bytes)}): {cmd_bytes}", flush=True)
         payload = bytearray()
         payload.append(0x00)
-        payload.extend(bytearray(list(cmd)))
+        payload.extend(cmd_bytes)
         ok: bool = self._i2c.write(self._addr, payload)
         assert ok
 
-    def data(self, data):
+    def data(self, data:List[int]):
         """Send data bytes."""
-        assert isinstance(data, bytearray)
+        assert isinstance(data, List)
+        assert isinstance(data[0], int)
+        print(f"data: {data} bytes", flush=True)
+        data = bytearray(data)
         i = 0
         n = len(data)
         while i < n:
@@ -42,6 +53,7 @@ class MyLumaSerial:
             payload.append(0x40)
             payload.extend(data[i : i + chunk_size])
             assert len(payload) == chunk_size + 1
+            print(f"data chunk payload ({chunk_size}): {payload}", flush=True)
             self._i2c.write(self._addr, payload)
             i += chunk_size
 
@@ -49,21 +61,20 @@ class MyLumaSerial:
 i2c = I2cAdapter(port="COM18")
 
 # Scan the I2C bus. 
-for adr in range(0, 127):
-    if i2c.write(adr, bytearray([0]), silent=True):
-        print(f"I2C response at 0x{adr:02x}")
+# for adr in range(0, 127):
+#     if i2c.write(adr, bytearray([0]), silent=True):
+#         print(f"I2C response at 0x{adr:02x}")
 
 # Create a Luma device.
-my_luma_serial = MyLumaSerial(i2c, 0x2c)                             
+my_luma_serial = MyLumaSerial(i2c, addr)                             
 device = sh1106(my_luma_serial, width=128, height=64, rotate = 0)
 
 # Draw.
-font_path = str(Path(__file__).resolve().parent.joinpath('fonts', 'C&C Red Alert [INET].ttf'))
-font2 = ImageFont.truetype(font_path, 12)
-with canvas(device) as draw:
+with canvas(device) as c:
     i = 0
     while True:
-      draw.text((0, 0), str(i), font=font2, fill="white")
+      print(f"Drawing {i}", flush=True)
+      c.rectangle(device.bounding_box, outline="white", fill="black")
       i += 1
-      time.sleep(0.5)
+      time.sleep(1.0)
     
